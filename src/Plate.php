011fcf00c22ff4735cd40ae6bf71f80faf04a846
sayadaazami <sayadaazami@gmail.com>
2016-11-02 08:57:20 +0330

    add getImage method

--- src/Plate.php
+++ src/Plate.php
@@ -5,18 +5,23 @@ use Plate\Exception\CharIsNotValid;
 use Plate\Exception\StateIsNotValid;
 use Plate\Exception\CityNotFound;
 use Plate\Exception\PlateIsNotValid;
+use Plate\FarsiGD;
 
 class Plate{
 	private $_plate = null;
 	private $_parsed = null;
 	private $_data = null;
 	private $_suportedChars = null;
+	private $_farsiGD = null;
+	private $_underLineChars = null;
 
 
 	public function __construct(){
 		$config = config('plate');
 		$this->_data = $config['state_data'];
 		$this->_suportedChars = $config['supported_chars'];
+		$this->_underLineChars = $config['under_line_chars'];
+		$this->_farsiGD = new FarsiGD();
 	}
 
 	private function parse(){
@@ -126,5 +131,40 @@ class Plate{
 
 	public function isCab(){
 		return $this->_parsed['type'] === 'تاکسی' ? true : false;
+	}
+
+	public function getImage($exportPath){
+		$data = $this->getparsedData();
+		$resourcePath = __DIR__ . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR;
+		$im = imagecreatefrompng($resourcePath . 'plate.png');
+		$font = $resourcePath . 'BTraffic.ttf';
+		$color = imagecolorallocate($im, 20, 20, 20);
+		
+		$this->_gdDrawText($data['stateNumber'], $im, $font, 48, $color, 268, 20); // draw state number
+		$underlineChars = ['','','','','','','','','','','','','','','',''];
+
+		$charLen = mb_strlen($data['char']);
+		$fontSize = $charLen == 3 ? 53 : 54;
+		$fontSizeChar = $charLen == 3 ? 12 : 7;
+		$y = $charLen == 3 ? 5 : 4;
+		$yChar = in_array($data['char'], $this->_underLineChars) ? $y - 4 : $y + ($charLen == 3 ? 12 : 9);
+		$x1 = $charLen == 3 ? 35 : 43;
+		$x2 = $x1 + ($charLen == 3 ? 54 : 58);
+		$x3 = $x2 + ($charLen == 3 ? 80 : 65);
+		$this->_gdDrawText($data['2DigitNumber'], 	$im, $font, $fontSize, $color, $x1, $y, false); // draw number
+		$this->_gdDrawText($data['char'], 			$im, $font, $fontSize - $fontSizeChar, $color, $x2, $yChar); // draw number
+		$this->_gdDrawText($data['3DigitNumber'], 	$im, $font, $fontSize, $color, $x3, $y, false); // draw number
+
+		imagepng($im, $exportPath);
+		imagedestroy($im);
+	}
+
+	private function _gdDrawText($text, $im, $font, $fontSize, $color, $x=0, $y=0, $rtl=true){
+		if($rtl){
+			$text = $this->_farsiGD->persianText($text, 'fa', 'normal');
+		}
+
+		$y += $fontSize;
+		imagettftext($im, $fontSize, 0, $x, $y , $color, $font, $text);
 	}
 }
